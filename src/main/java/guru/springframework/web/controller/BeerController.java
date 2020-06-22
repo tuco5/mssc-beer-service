16f8ec29125d99b9ae5f616067d3bf3369383177
Tudor Comparan <tudor.comparan@gmail.com>
2020-06-22 15:09:00 -0500

    Adding beer controller. closes #3

---
+++ src/main/java/guru/springframework/web/controller/BeerController.java
@@ -0,0 +1,41 @@
+package guru.springframework.web.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import guru.springframework.web.model.BeerDto;
+
+@RestController
+@RequestMapping("api/v1/beer")
+public class BeerController {
+	
+	@GetMapping("/{beerId")
+	public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId){
+		
+		// TODO implementation
+		return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+		
+		// TODO implementation
+		return new ResponseEntity(HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{beerId}")
+	public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+		
+		// TODO implementation
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
+}
